94d4c4f7b403f13a75e5826bc5d293a6a72e5189
Eluriel <benji91@netplus.ch>
2025-07-14 12:01:10 +0200

    s4/66: Adding Section component and forwarding props

--- react-landing-page/src/components/TabButton.jsx
+++ react-landing-page/src/components/TabButton.jsx
@@ -1,7 +1,7 @@
-function TabButton({ children, onSelect, isSelected }) {
+function TabButton({ children, isSelected, ...props }) {
   return (
     <li>
-      <button className={isSelected ? 'active' : null} onClick={onSelect}>
+      <button className={isSelected ? 'active' : null} {...props}>
         {children}
       </button>
     </li>
