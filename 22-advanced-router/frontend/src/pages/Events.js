6ffe73e5138a3573e084e4855a1e94b9470673d4
Eluriel <benji91@netplus.ch>
2025-10-28 10:22:40 +0100

    s22: Add ErrorPage component and update routing in App.js; enhance error handling in EventsPage

--- 22-advanced-router/frontend/src/pages/Events.js
+++ 22-advanced-router/frontend/src/pages/Events.js
@@ -2,13 +2,14 @@ import EventsList from '../components/EventsList';
 import { useLoaderData } from 'react-router-dom';
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
 
-  return (
-    <>
-      <EventsList events={events} />
-    </>
-  );
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+
+  const events = data.events;
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
@@ -17,9 +18,11 @@ export async function loader() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: 'Could not fetch events.' };
+    throw new Response(JSON.stringify({ message: 'Could not fetch events!' }), {
+      status: 500,
+    });
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
