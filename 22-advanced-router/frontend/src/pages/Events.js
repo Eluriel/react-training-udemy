c13a4dbaeca20ed4910e842a43511ce38e0965ca
Eluriel <benji91@netplus.ch>
2025-10-30 10:26:33 +0100

    s22: Refactor NewsletterSignup and EventsPage components; integrate useFetcher for form handling and enhance event loading with Suspense and Await

--- 22-advanced-router/frontend/src/pages/Events.js
+++ 22-advanced-router/frontend/src/pages/Events.js
@@ -1,20 +1,22 @@
+import { Suspense } from 'react';
 import EventsList from '../components/EventsList';
-import { useLoaderData } from 'react-router-dom';
+import { Await, useLoaderData } from 'react-router-dom';
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
 
-  if (data.isError) {
-    return <p>{data.message}</p>;
-  }
-
-  const events = data.events;
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
@@ -23,6 +25,13 @@ export async function loader() {
       status: 500,
     });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export async function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
