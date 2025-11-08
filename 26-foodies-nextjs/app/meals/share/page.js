84b7b0b097c625d58af2f7deb9fbe3ba33729637
Eluriel <benji91@netplus.ch>
2025-11-08 16:38:56 +0100

    s26: Handle submitting of the form

--- 26-foodies-nextjs/app/meals/share/page.js
+++ 26-foodies-nextjs/app/meals/share/page.js
@@ -1,4 +1,5 @@
 import ImagePicker from "@/components/Meals/ImagePicker";
+import MealsFormSubmit from "@/components/Meals/MealsFormSubmit";
 import { shareMeal } from "@/lib/actions";
 
 import classes from "./page.module.css";
@@ -43,7 +44,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label="Meal Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
