a65109c06a5202446b9d6fd1a59ab804953bf4a7
Eluriel <benji91@netplus.ch>
2025-11-08 13:42:27 +0100

    s26: Add meal detail page

--- 26-foodies-nextjs/app/meals/[mealSlug]/page.js
+++ 26-foodies-nextjs/app/meals/[mealSlug]/page.js
@@ -1,13 +1,33 @@
-import Link from "next/link";
+import Image from "next/image";
+
+import { getMeal } from "@/lib/meals";
+
+import classes from "./page.module.css";
 
 export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
   return (
-    <main>
-      <h1>Meal Details</h1>
-      <p>{params.mealSlug}</p>
-      <p>
-        <Link href="/meals">Back to Meals</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
   );
 }
