1a245e1da4d2f3b97e51340d26931ecf026c9ef8
Eluriel <benji91@netplus.ch>
2025-11-18 11:04:47 +0100

    s27: Add various files used for the training
    Nothing really exciting here...

--- 27-react-server-components/app/page.js
+++ 27-react-server-components/app/page.js
@@ -1,7 +1,34 @@
-export default function Home() {
+import { Suspense } from 'react';
+import fs from 'node:fs/promises';
+
+import ClientDemo from '@/components/ClientDemo';
+import DataFetchingDemo from '@/components/DataFetchingDemo';
+import RSCDemo from '@/components/RSCDemo';
+import ServerActionsDemo from '@/components/ServerActionsDemo';
+import UsePromiseDemo from '@/components/UsePromisesDemo';
+import ErrorBoundary from '@/components/ErrorBoundary';
+
+export default async function Home() {
+  const fetchUsersPromise = new Promise((resolve, reject) => {
+    setTimeout(async () => {
+      const data = await fs.readFile('dummy-db.json', 'utf-8');
+      const users = JSON.parse(data);
+      resolve(users);
+      // reject(new Error('ERROR!'));
+    }, 2000);
+  });
   return (
     <main>
-      <p>Let's go!</p>
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={fetchUsersPromise} />
+        </Suspense>
+      </ErrorBoundary>
+      <ClientDemo>
+        <RSCDemo />
+      </ClientDemo>
+      <DataFetchingDemo />
+      <ServerActionsDemo />
     </main>
   );
 }
