c64fb2c19878f89becb811a6a65a1dc42945c8d5
Eluriel <benji91@netplus.ch>
2025-11-19 15:26:14 +0100

    s29: Implement Accordion and AccordionItem components for enhanced content display

---
+++ 29-react-patterns/src/components/Accordion/Accordion.jsx
@@ -0,0 +1,33 @@
+import { useContext } from 'react';
+import { useState } from 'react';
+import { createContext } from 'react';
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+
+  if (!ctx) {
+    throw new Error('Accordion-related must be wrapped by <Accordion>.');
+  }
+
+  return ctx;
+}
+
+export default function Accordion({ children, className }) {
+  const [openItemId, setOpenItemId] = useState();
+
+  function toggleItem(id) {
+    setOpenItemId((prevId) => (prevId === id ? null : id));
+  }
+
+  const contextValue = {
+    openItemId,
+    toggleItem,
+  };
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>{children}</ul>
+    </AccordionContext.Provider>
+  );
+}
